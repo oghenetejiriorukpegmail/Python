1ec004426ba4fce0f6c7d76bd275e095e409be59
miner <miner@DESKTOP-ORUKPE>
2022-04-22 17:19:22 -0400

    added website clicker

--- Website Clicker/routine_clicks.py
+++ Website Clicker/routine_clicks.py
@@ -18,17 +18,30 @@ def clicker(element_locator):
         print(exception)
 
 def login(param,xpath):
-    credential=driver.find_element_by_xpath(xpath)
+    credential=driver.find_element(By.XPATH, xpath)
     credential.send_keys(param)
 
 config=ConfigParser()
 config.read('Website Clicker/config.ini')
 #print(config.sections())
-websites=config['Websites']['cryptostackers']
+cryptostackers=config['Websites']['cryptostackers']
+coinstats=config['Websites']['coinstats']
 
 driver=webdriver.Chrome()
 driver.implicitly_wait(100)
-driver.get(websites)
+driver.get(coinstats)
+
+clicker('//*[@id="__next"]/main/header/div/div[2]/div[3]/a[1]')
+login(config['Credentials']['username'],'//*[@id="__next"]/main/div[2]/div/form/div[1]/div[1]/div/input')
+login(config['Credentials']['password'],'//*[@id="__next"]/main/div[2]/div/form/div[1]/div[2]/div/input')
+time.sleep(1)
+clicker('//*[@id="__next"]/main/div[2]/div/form/div[2]/button')
+time.sleep(5)
+
+driver.close()
+
+time.sleep(10)
+driver.get(cryptostackers)
 
 clicker('/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[1]/div[4]/div/button')
 clicker('/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[2]/div/div')
@@ -36,7 +49,7 @@ clicker('/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[2
 login(config['Credentials']['username'],'/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/input')
 login(config['Credentials']['password'],'/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/input')
 
-#time.sleep(1)
+time.sleep(1)
 
 clicker('/html/body/div[1]/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[4]/div/button')
 clicker('//*[@id="confetti"]')
@@ -44,6 +57,11 @@ clicker('//*[@id="confetti"]')
 time.sleep(10)
 
 driver.close()
+
+
+
+
+
 
 '''
 config['Websites'] = {
